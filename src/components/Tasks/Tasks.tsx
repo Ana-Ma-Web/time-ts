321fab2e0f620df32dd14256eb8a645d42000f9d
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2022-10-29 19:25:22 +0300

    keyDownHandler

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import type { RootState } from '../../redux/store'
 import { useSelector, useDispatch } from 'react-redux'
 
-import {taskAdd} from './../../redux/slices/tasksSlice'
+import { taskAdd } from './../../redux/slices/tasksSlice'
 
 import { TaskType } from '../..';
 import Widget from '../Widget/Widget';
@@ -28,25 +28,41 @@ function Tasks() {
 
    const createNewTask = (name: string, color: string) => {
       if (name !== '') {
-      dispatch(taskAdd({name, color}))
-      inputClear()}
+         dispatch(taskAdd({ name, color }))
+         inputClear()
+      }
    }
 
-return (
+   const keyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      switch (event.code) {
+         case 'Enter':
+            createNewTask(text, 'white')
+            break;
+         case 'Escape':
+            inputClear()
+            break;
+         default:
+            break;
+      }
+
+   }
+
+   return (
       <div className='Tasklist block'>
          {/* <Widget iconsActs={iconsInWidget} /> */}
          <ul className='Tasklist_list'>
             {
                tasks.map((item: TaskType) => (
                   <div key={item.date}>
-                     <Task task={item} inputText={text} inputClear={inputClear}/>
+                     <Task task={item} inputText={text} inputClear={inputClear} />
                   </div>
                ))
             }
          </ul>
          <label>
             <input value={text} onChange={(e) => setText(e.target.value)}
-            type="text" placeholder="add task" />
+               onKeyDown={keyDownHandler}
+               type="text" placeholder="add task" />
             <button onClick={() => createNewTask(text, 'white')}>Add task</button>
          </label>
       </div>
